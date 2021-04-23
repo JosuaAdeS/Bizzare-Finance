0bdad0d288fecd7d3b7eef957a275044e4b8e3ae
josuaade <672018272@student.uksw.edu>
2021-04-23 19:25:53 +0700

    Add Register Interface and Refine Login Interface

--- app/src/main/java/com/example/bizzarefinance/MainActivity.java
+++ app/src/main/java/com/example/bizzarefinance/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.bizzarefinance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String kepo;
+    //private String kepo;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = findViewById(R.id.btnRegis);
+    }
+
+    public void register (View v){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
