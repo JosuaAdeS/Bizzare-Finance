8e8e58fbffa19ec7e9f26838d25b11849917b7e1
felikdw <dwfelik@gmail.com>
2021-04-25 21:32:37 +0700

    add TradeActivity and BuySellActivity

--- app/src/main/java/com/example/bizzarefinance/TradeActivity.java
+++ app/src/main/java/com/example/bizzarefinance/TradeActivity.java
@@ -2,13 +2,53 @@ package com.example.bizzarefinance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class TradeActivity extends AppCompatActivity {
+
+    Intent buytent,market,wallettent,selltent;
+    ImageButton buy,wallet,sell;
+    ImageView trade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade);
+
+        buy = findViewById(R.id.imaget_btn_trade_buy);
+        trade = findViewById(R.id.marketbtn);
+        wallet = findViewById(R.id.navWallet1);
+        selltent = new Intent(this,BuySellActivity.class);g
+        buytent = new Intent(this, BuySellActivity.class);
+        market = new Intent(this,MarketActivity.class);
+        wallettent = new Intent(this,WalletActivity.class);
+
+        wallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(wallettent);
+            }
+        });
+
+        trade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(market);
+            }
+        });
+
+
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(buytent);
+            }
+        });
+
+
     }
 }
